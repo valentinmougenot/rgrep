62149d1687a92dee4a53cb3e00e6f98b810262ed
valentinmougenot <valentin.mougenot2003@outlook.com>
2026-08-06 21:21:40 +0200

    feat(regex-engine): implement Display for ParseError

--- crates/regex-engine/src/error.rs
+++ crates/regex-engine/src/error.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct ParseError {
     pub kind: ParseErrorKind,
@@ -12,4 +14,29 @@ pub enum ParseErrorKind {
     EmptyCharClass,
     InvalidRange { start: char, end: char },
     DanglingRepetitionOperator,
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self.kind {
+            ParseErrorKind::UnexpectedEof => write!(f, "Unexpected EOF at position {}", self.pos),
+            ParseErrorKind::UnclosedGroup => write!(f, "Unclosed group at position {}", self.pos),
+            ParseErrorKind::UnclosedCharClass => {
+                write!(f, "Unclosed char class at position {}", self.pos)
+            }
+            ParseErrorKind::EmptyCharClass => {
+                write!(f, "Empty char class at position {}", self.pos)
+            }
+            ParseErrorKind::InvalidRange { start, end } => {
+                write!(
+                    f,
+                    "Invalid range '{}-{}' at position {}",
+                    start, end, self.pos
+                )
+            }
+            ParseErrorKind::DanglingRepetitionOperator => {
+                write!(f, "Dangling repetition operator at position {}", self.pos)
+            }
+        }
+    }
 }
